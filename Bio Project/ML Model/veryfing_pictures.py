bdfda90c868f546617e61b49c118ca636746e34a
Joao Guilherme Bruni Cataneo da Silva <118385255+joao10guilherme@users.noreply.github.com>
2026-02-12 22:18:31 -0500

    Refactor model loading and prediction for live feed
    Updated the model loading and prediction functions to use OpenCV for image processing. Enhanced device compatibility and added live feed functionality for waste classification.

--- Bio Project/ML Model/veryfing_pictures.py
+++ Bio Project/ML Model/veryfing_pictures.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import cv2 as cv
 from torchvision import models, transforms
 from PIL import Image
 
@@ -9,8 +10,11 @@ num_classes = len(classes)
 
 # 2. Re-create the Architecture -> identical to the trained model
 def load_trained_model(weights_path):
-    model = models.mobilenet_v3_small(weights=None)
 
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    model = models.mobilenet_v3_small(weights=None)
     # Custom classifier
     model.classifier = nn.Sequential(
         nn.Linear(model.classifier[0].in_features, 256),
@@ -20,14 +24,16 @@ def load_trained_model(weights_path):
     )
 
     # Load the local file
-    model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+    model.to(device)
     model.eval() # set to evaluation mode
-    return model
+    return model, device
 
 # 3. Set up the preprocessing -> transforms, the same from val_transform
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 transform = transforms.Compose([
+    transforms.ToPILImage(),
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
@@ -35,55 +41,59 @@ transform = transforms.Compose([
 ])
 
 # 4. prediction function
-def predict(image_path, model):
-    image = Image.open(image_path).convert('RGB')
-    image.show()
-    image = transform(image).unsqueeze(0)
+def predict(frame, model, device):
+    # Convert BGR (OpenCV) to RGB(PIL/Torch)
+    frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    #Apply transforms and move to device
+    input_tensor = transform(frame_rgb).unsqueeze(0).to(device)
 
     with torch.no_grad():
-        outputs = model(image)
+        outputs = model(input_tensor)
         _, predicted = torch.max(outputs, 1)
     
     return classes[predicted.item()]
 
 # -------------------------------------
-# Returning: Recyling, Waste, or reject
+# Returning: Recyling, Waste, or Reject
 # -------------------------------------
 def waste_classfication_decision(result):
 
     recycle = ['cardboard', 'clothes', 'glass', 'metal', 'paper', 'plastic', 'shoes']
     waste = ['biological', 'trash']
-    reject = 'battery'
-
-    # 1 -> recycle
-    # 2-> waste
-    # 3-> reject
-
-    answer = 0
-
-    if result.lower() in recycle:
-        answer = 1
     
-    if result.lower() in waste:
-        answer = 2
+    if result.lower() in recycle: return "RECYCLABLE"
+    if result.lower() in waste: return "WASTE"
+    if result.lower() == 'battery': return "REJECT (BATTERY)"
+    return "UNKNOWN"
+
+# --------------------------------
+# Cellphone Camera
+# --------------------------------
+model, device = load_trained_model("/Users/guicataneo/Desktop/Bio Project/ML Model/waste_predict_model.pth")
+
+cap = cv.VideoCapture(0)
+print("Starting live feed... Press 'q' to quit.")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Camera not found...")
+        break
     
-    if result.lower() == reject:
-        answer = 3
+    # ------------------
+    # Execution
+    # ------------------
+    category = predict(frame, model, device)
+    decision = waste_classfication_decision(category)
 
-    return answer
-    
-# ------------------
-# Execution
-# ------------------
-model = load_trained_model("/Users/guicataneo/Desktop/Bio Project/ML Model/waste_predict_model.pth")
-result = predict("/Users/guicataneo/Downloads/plastic_bottle.jpg", model)
-print(f"The predicted waste category is: {result}")
-answer = waste_classfication_decision(result)
+    display_text = f"{category.upper()} -> {decision}"
+    cv.putText(frame, display_text, (30, 60), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), 2)
 
+    cv.imshow('Waste Classifirer AI', frame)
 
-if answer == 1:
-    print("This is: Recyclabel")
-elif answer == 2:
-    print("This is: Waste")
-elif answer == 3:
-    print("This is: Reject")
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
